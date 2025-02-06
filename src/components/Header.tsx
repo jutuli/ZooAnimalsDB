55f0fe21d54ce88638f98f8d7550a10d8c90526d
Julia T <juliaturkali@gmail.com>
2025-02-06 13:13:22 +0100

    Add Header Component

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header className="my-10 flex flex-col items-center gap-5">
+      <h1 className="text-6xl font-bold tracking-wide text-gray-300">
+        ZooPartyDB
+      </h1>
+      <h2 className="text-2xl font-semibold tracking-wide text-gray-400">
+        Explore the fascinating World of Zoo Animals!
+      </h2>
+    </header>
+  );
+}
+
+export default Header;
